930f880d566348132c02e5e72ce03a92362558f5
𝙴𝚖𝚙𝚒𝚛𝚎 𝚃𝚎𝚌𝚑 <151479661+efeurhobobullish@users.noreply.github.com>
2025-05-15 02:40:46 +0100

    Update gold_vip.js

--- plugins/gold_vip.js
+++ plugins/gold_vip.js
@@ -17,32 +17,3 @@ module.exports = (bot) => {
     });
   });
 };
-
-bot.action("subscribe_1m", async (ctx) => {
-  try {
-    await ctx.editMessageMedia(
-      {
-        type: "photo",
-        media: "https://files.catbox.moe/tg23p6.png",
-      },
-      {
-        caption:
-          "I have a SPECIAL OFFER for you🎁\n\n" +
-          "You will get:\n\n" +
-          "💎3+ signals daily\n" +
-          "💎Entry, Stop Loss\n" +
-          "💎One Take Profit\n" +
-          "💎Forex, Gold, Oil, Indexes,
-        parse_mode: "Markdown",
-        reply_markup: {
-          inline_keyboard: [
-            [{ text: "Pay $99 (Regular Price)", callback_data: "pay_99" }],
-            [{ text: "« Back", callback_data: "buyGoldVIP" }],
-          ],
-        },
-      }
-    );
-  } catch (error) {
-    console.error("Error sending subscription offer:", error);
-  }
-});
