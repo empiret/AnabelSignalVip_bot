9e557b93b923a4f5d9337c7c87255559696e364a
𝙴𝚖𝚙𝚒𝚛𝚎 𝚃𝚎𝚌𝚑 <151479661+efeurhobobullish@users.noreply.github.com>
2025-05-15 20:40:17 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,7 @@
 module.exports = {
         BOT_TOKEN: '7855368643:AAGSfLb8r3iLmPiWdOUmh58bEfrnSH_6RpM',
         ADMIN_ID: '6307156448',
+        VIP_GROUP_LINK_99: 'https://t.me/empire_tech_update",
+        VIP_GROUP_LINK_199: 'https://t.me/empire_tech_update",
+        VIP_GROUP_LINK_299: 'https://t.me/empire_tech_update",
 };
