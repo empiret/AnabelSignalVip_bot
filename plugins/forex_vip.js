364e49f27579ff01705be96518723a875bbf36a3
𝙴𝚖𝚙𝚒𝚛𝚎 𝚃𝚎𝚌𝚑 <151479661+efeurhobobullish@users.noreply.github.com>
2025-05-15 02:08:48 +0100

    Create forex_vip.js

---
+++ plugins/forex_vip.js
@@ -0,0 +1,22 @@
+module.exports = (bot) => {
+  bot.action('forex_vip', async (ctx) => {
+    await ctx.answerCbQuery();
+
+    await ctx.replyWithPhoto(
+      { url: 'https://files.catbox.moe/sx1d6k.jpg' }, // replace with your actual image URL if different
+      {
+        caption:
+          '*FOREX SIGNALS VIP* Subscription Options:\n\nPlease select a subscription package:',
+        parse_mode: 'Markdown',
+        reply_markup: {
+          inline_keyboard: [
+            [{ text: '1 Month Package - $99.0', callback_data: 'forex_1month' }],
+            [{ text: '6 Month Package - $199.0', callback_data: 'forex_6month' }],
+            [{ text: '1 Year Plan - $299.0', callback_data: 'forex_1year' }],
+            [{ text: '« Back to Categories', callback_data: 'back_to_start' }]
+          ]
+        }
+      }
+    );
+  });
+};
