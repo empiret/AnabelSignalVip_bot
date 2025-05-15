a2b8865904feff0091751a8b0a825ce3032e7d11
𝙴𝚖𝚙𝚒𝚛𝚎 𝚃𝚎𝚌𝚑 <151479661+efeurhobobullish@users.noreply.github.com>
2025-05-15 01:59:50 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
-const { Telegraf } = require("telegraf");
-const fs = require("fs");
-const path = require("path");
+const { Telegraf } = require('telegraf');
+const fs = require('fs');
+const path = require('path');
 
-const bot = new Telegraf("7771249915:AAHIOcUzHBQEpvrL1nzgMSa81CoXntmySGg");
+const bot = new Telegraf('7835282217:AAH19DC8yxQ4dtIJh9qFNQPMKgixNL2KR90'); 
 
-// Auto-load plugins
-fs.readdirSync("./plugins").forEach((file) => {
+// Load all plugins
+const pluginsPath = path.join(__dirname, 'plugins');
+fs.readdirSync(pluginsPath).forEach(file => {
   const plugin = require(`./plugins/${file}`);
-  if (typeof plugin === "function") plugin(bot);
+  if (typeof plugin === 'function') {
+    plugin(bot);
+  }
 });
 
 bot.launch();
-console.log("Bot started...");
+console.log('Bot is running...');
