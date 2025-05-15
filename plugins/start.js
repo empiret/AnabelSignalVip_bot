d5b7663d5f1805962c240487d22dbc3c82a3f16c
𝙴𝚖𝚙𝚒𝚛𝚎 𝚃𝚎𝚌𝚑 <151479661+efeurhobobullish@users.noreply.github.com>
2025-05-15 02:33:27 +0100

    Update start.js

--- plugins/start.js
+++ plugins/start.js
@@ -6,26 +6,25 @@ module.exports = (bot) => {
         caption:
           "*Guys,*\n" +
           "*Welcome to AnabelSignals Payment Bot*\n\n" +
-          "This bot will help you to purchase VIP signals subscription easily!\n\n" +
-          "*Check the available payment options:*\n" +
-          "🌸 Credit / debit card\n" +
-          "🌸 Apple pay / Samsung pay\n" +
-          "🌸 Crypto currencies\n\n" +
-          "*To buy a VIP subscription:*\n" +
-          "🤔 Choose a subscription plan below\n" +
-          "🛍 Get a personal payment link\n" +
-          "💳 Pay with preferred method\n" +
-          "🎉 Get your invitation link\n\n" +
-          "*❓Need Help*\n" +
-          "📲 [@laxtechbot](https://t.me/laxtechbot)",
+          "This bot will help you purchase VIP signals subscription easily!\n\n" +
+          "*Payment Options:*\n" +
+          "🌸 Credit/Debit Card\n" +
+          "🌸 Apple Pay/Samsung Pay\n" +
+          "🌸 Crypto Currencies\n\n" +
+          "*To Buy VIP:*\n" +
+          "1. Choose a plan\n" +
+          "2. Get your payment link\n" +
+          "3. Pay\n" +
+          "4. Receive invitation link\n\n" +
+          "*Need Help?* Contact: @laxtechbot",
         parse_mode: "Markdown",
         reply_markup: {
           inline_keyboard: [
-            [{ text: "FOREX SIGNALS VIP", callback_data: "buy_forex" }],
-            [{ text: "GOLD SIGNALS VIP", callback_data: "buy_gold" }],
-            [{ text: "ACCOUNT MANAGEMENT SCHEME", callback_data: "buy_crypto" }]
-          ]
-        }
+            [{ text: "FOREX SIGNALS VIP", callback_data: "buyForexVIP" }],
+            [{ text: "GOLD SIGNALS VIP", callback_data: "buyGoldVIP" }],
+            [{ text: "ACCOUNT MANAGEMENT SCHEME", callback_data: "buyCryptoVIP" }],
+          ],
+        },
       }
     );
   });
