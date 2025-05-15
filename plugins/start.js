ab476f6f8adb4b3dd185b59133dc0baa0fd90197
𝙴𝚖𝚙𝚒𝚛𝚎 𝚃𝚎𝚌𝚑 <151479661+efeurhobobullish@users.noreply.github.com>
2025-05-15 01:41:22 +0100

    Update start.js

--- plugins/start.js
+++ plugins/start.js
@@ -1,4 +1,37 @@
 module.exports = (bot) => {
+  const sendStartMessage = (ctx) => {
+    return ctx.editMessageMedia(
+      {
+        type: "photo",
+        media: "https://files.catbox.moe/tg23p6.png",
+        caption:
+          "*Guys,*\n" +
+          "*Welcome to AnabelSignals Payment Bot*\n\n" +
+          "This bot will help you purchase VIP signals subscription easily!\n\n" +
+          "*Payment Options:*\n" +
+          "🌸 Credit/Debit Card\n" +
+          "🌸 Apple Pay/Samsung Pay\n" +
+          "🌸 Crypto Currencies\n\n" +
+          "*To Buy VIP:*\n" +
+          "1. Choose a plan\n" +
+          "2. Get your payment link\n" +
+          "3. Pay\n" +
+          "4. Receive invitation link\n\n" +
+          "*Need Help?* Contact: @laxtechbot",
+        parse_mode: "Markdown",
+      },
+      {
+        reply_markup: {
+          inline_keyboard: [
+            [{ text: "FOREX SIGNALS VIP", callback_data: "buyForexVIP" }],
+            [{ text: "GOLD SIGNALS VIP", callback_data: "buyGoldVIP" }],
+            [{ text: "ACCOUNT MANAGEMENT SCHEME", callback_data: "buyCryptoVIP" }],
+          ],
+        },
+      }
+    );
+  };
+
   bot.start((ctx) => {
     ctx.replyWithPhoto(
       { url: "https://files.catbox.moe/tg23p6.png" },
@@ -27,5 +60,9 @@ module.exports = (bot) => {
         },
       }
     );
+  });
+
+  bot.action("start", (ctx) => {
+    sendStartMessage(ctx);
   });
 };
