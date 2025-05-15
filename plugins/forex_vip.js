222e6781d6fbd6d33a4ce4ab2ff0703194b6b9b2
𝙴𝚖𝚙𝚒𝚛𝚎 𝚃𝚎𝚌𝚑 <151479661+efeurhobobullish@users.noreply.github.com>
2025-05-15 02:33:59 +0100

    Update forex_vip.js

--- plugins/forex_vip.js
+++ plugins/forex_vip.js
@@ -1,19 +1,19 @@
 module.exports = (bot) => {
-  bot.action("buy_forex", (ctx) => {
-    ctx.replyWithPhoto(
-      { url: "https://files.catbox.moe/tg23p6.png" },
-      {
-        caption: "*FOREX SIGNALS VIP* Subscription Options:\n\nPlease select a subscription package:",
-        parse_mode: "Markdown",
-        reply_markup: {
-          inline_keyboard: [
-            [{ text: "1 Month - $99", callback_data: "plan_forex_1m" }],
-            [{ text: "6 Months - $199", callback_data: "plan_forex_6m" }],
-            [{ text: "1 Year - $299", callback_data: "plan_forex_1y" }],
-            [{ text: "« Back", callback_data: "start_over" }]
-          ]
-        }
+  bot.action("buyForexVIP", (ctx) => {
+    ctx.editMessageMedia({
+      type: "photo",
+      media: "https://files.catbox.moe/tg23p6.png",
+      caption: "*FOREX SIGNALS VIP*\n\nChoose a subscription:",
+      parse_mode: "Markdown",
+    }, {
+      reply_markup: {
+        inline_keyboard: [
+          [{ text: "1 Month - $99", callback_data: "pay_forex_1m" }],
+          [{ text: "6 Months - $199", callback_data: "pay_forex_6m" }],
+          [{ text: "1 Year - $299", callback_data: "pay_forex_1y" }],
+          [{ text: "« Back", callback_data: "start" }],
+        ],
       }
-    );
+    });
   });
 };
