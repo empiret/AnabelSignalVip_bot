96fdc4219e6449f5773315f7b5d5fd01c3cc20c0
𝙴𝚖𝚙𝚒𝚛𝚎 𝚃𝚎𝚌𝚑 <151479661+efeurhobobullish@users.noreply.github.com>
2025-05-15 02:47:06 +0100

    Create pay_99.js

---
+++ plugins/pay_99.js
@@ -0,0 +1,21 @@
+module.exports = (bot) => {
+  bot.action("pay_99", async (ctx) => {
+    try {
+      await ctx.editMessageText(
+        "Choose payment method\n\n" +
+        "You can pay with VISA/MasterCard or with Cryptocurrency.",
+        {
+          reply_markup: {
+            inline_keyboard: [
+              [{ text: "Pay with VISA/MasterCard", callback_data: "pay_visa" }],
+              [{ text: "Pay with Cryptocurrency", callback_data: "pay_crypto" }]
+            ],
+          },
+          parse_mode: "Markdown"
+        }
+      );
+    } catch (error) {
+      console.error("Error in pay_99 callback:", error);
+    }
+  });
+};
