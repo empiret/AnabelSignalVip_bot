7ac01422d30547d7318d29a2eb317dca1e6e30dc
𝙴𝚖𝚙𝚒𝚛𝚎 𝚃𝚎𝚌𝚑 <151479661+efeurhobobullish@users.noreply.github.com>
2025-05-15 03:18:24 +0100

    Create pay_299.js

---
+++ plugins/pay_299.js
@@ -0,0 +1,24 @@
+module.exports = (bot) => {
+  bot.action("pay_299", async (ctx) => {
+    try {
+      await ctx.answerCbQuery(); // To remove loading spinner
+
+      await ctx.telegram.sendMessage(
+        ctx.chat.id,
+        "Choose payment method\n\n" +
+        "You can pay with VISA/MasterCard or with Cryptocurrency.",
+        {
+          reply_markup: {
+            inline_keyboard: [
+              [{ text: "Pay with VISA/MasterCard", callback_data: "pay_visa" }],
+              [{ text: "Pay with Cryptocurrency", callback_data: "pay_crypto299" }]
+            ],
+          },
+          parse_mode: "Markdown"
+        }
+      );
+    } catch (error) {
+      console.error("Error in pay_99 callback:", error);
+    }
+  });
+};
