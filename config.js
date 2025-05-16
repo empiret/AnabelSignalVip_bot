40a508e4a63c237fc556601ccc6a37e223d3092e
efeurhobobullish <efeurhobobullish@gmail.com>
2025-05-16 20:23:23 +0100

    Remove all files

--- config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-        BOT_TOKEN: '7629822251:AAHqLF8jgv49Iz8TfRGgEzVxwipg9ouB7UQ',
-        ADMIN_ID: '6307156448',
-        VIP_GROUP_LINK_99: 'https://t.me/empire_tech_update',
-        VIP_GROUP_LINK_199: 'https://t.me/empire_tech_update',
-        VIP_GROUP_LINK_299: 'https://t.me/empire_tech_update',
-};
-
