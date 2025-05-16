38ec4dbfa339e82fbe2fdebfa179a9478cbc2d39
𝙴𝚖𝚙𝚒𝚛𝚎 𝚃𝚎𝚌𝚑 <151479661+efeurhobobullish@users.noreply.github.com>
2025-05-16 20:33:19 +0100

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const { Telegraf } = require("telegraf");
 const fs = require("fs");
 const path = require("path");
 
-const bot = new Telegraf("8026096500:AAEBGmTHKIQnVX4vyIcmFGootx1DCSZD_Rg");
+const bot = new Telegraf("7964318828:AAGyYkiEWtK5aDkaps-7X8qn3-CKJlA7ycU");
 console.log("Installing plugins...");
 // Auto-load all command files
 fs.readdirSync(path.join(__dirname, "plugins")).forEach((file) => {
