b134eed271abdfeadc889514e4435e918421a8b7
𝙴𝚖𝚙𝚒𝚛𝚎 𝚃𝚎𝚌𝚑 <151479661+efeurhobobullish@users.noreply.github.com>
2025-05-16 21:24:35 +0100

    Update app.js

--- app.js
+++ app.js
@@ -2,15 +2,27 @@ const axios = require("axios");
 const { Telegraf } = require("telegraf");
 const fs = require("fs");
 const path = require("path");
+const express = require("express");
 
 const bot = new Telegraf("7964318828:AAGyYkiEWtK5aDkaps-7X8qn3-CKJlA7ycU");
+
+// Load plugins
 console.log("Installing plugins...");
-// Auto-load all command files
 fs.readdirSync(path.join(__dirname, "plugins")).forEach((file) => {
   const plugin = require(`./plugins/${file}`);
   if (typeof plugin === "function") plugin(bot);
 });
-console.log("plugins installed..");
+console.log("Plugins installed.");
 
+// Start bot
 bot.launch();
 console.log("Bot is running...");
+
+// Keep alive server
+const app = express();
+app.get("/", (req, res) => res.send("Bot is alive!"));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Keep-alive server running on port ${PORT}`);
+});
