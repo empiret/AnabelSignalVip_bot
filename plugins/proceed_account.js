3d6c752b7c31b9a71d3cf8e655a1fdd82655b0f0
𝙴𝚖𝚙𝚒𝚛𝚎 𝚃𝚎𝚌𝚑 <151479661+efeurhobobullish@users.noreply.github.com>
2025-05-18 02:11:34 +0100

    Update proceed_account.js

--- plugins/proceed_account.js
+++ plugins/proceed_account.js
@@ -3,26 +3,23 @@ module.exports = (bot) => {
     try {
       await ctx.answerCbQuery(); // Acknowledge the callback to avoid loading animation
 
-      const adminLink = "https://t.me/AnabelSignalContact1"; // Fixed missing 'h' in URL
+      const adminLink = "https://t.me/AnabelSignalContact1";
 
-      await ctx.editMessageMedia(
-        {
-          type: "photo",
-          media: "https://files.catbox.moe/25rj59.jpg",
-          caption:
-            "Dear user kindly drop YOUR KYC DETAILS:\n\n" +
-            "*NAME*\n*RESIDENTIAL ADDRESS*\n*GENDER*\n*DATE OF BIRTH (AGE)*",
-          parse_mode: "Markdown",
+      await ctx.editMessageMedia({
+        type: "photo",
+        media: "https://files.catbox.moe/25rj59.jpg",
+        caption:
+          "Dear user kindly drop YOUR KYC DETAILS:\n\n" +
+          "*NAME*\n*RESIDENTIAL ADDRESS*\n*GENDER*\n*DATE OF BIRTH (AGE)*",
+        parse_mode: "Markdown",
+      }, {
+        reply_markup: {
+          inline_keyboard: [
+            [{ text: "Send KYC", web_app: { url: adminLink } }],
+            [{ text: "« Back", callback_data: "account_schemes" }],
+          ],
         },
-        {
-          reply_markup: {
-            inline_keyboard: [
-              [{ text: "Send KYC", web_app: { url: adminLink } }],
-              [{ text: "« Back", callback_data: "account_schemes" }],
-            ],
-          },
-        }
-      );
+      });
     } catch (error) {
       console.error("Error handling proceed_account callback:", error);
     }
